d586ac2115f5f49f8afb6484143041f9a3d2d2f2
huseyinkedik <159181717+huseyinkedik@users.noreply.github.com>
2024-02-13 09:02:59 +0300

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+ class Main {
+    public static void main(String[] args) {
+        double yarıcap, alan, cevre, pi = 3.14;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yarıçap değerini giriniz: ");
+        yarıcap = input.nextInt();
+
+        alan = pi * yarıcap * yarıcap;
+        cevre = 2 * pi * yarıcap;
+
+        System.out.println("Alan: " + alan);
+        System.out.println("Çevre: " + cevre);
+
+
+}
 }
